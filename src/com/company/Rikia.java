3ae12356d567e0bec13202f6e787842cb4accfe0
Radhe1993 <rozyom91@gmail.com>
2023-09-10 15:27:21 +0100

    test002

--- src/com/company/Rikia.java
+++ src/com/company/Rikia.java
@@ -4,5 +4,6 @@ package com.company;
 public class Rikia {
     public static void main(String[] args) {
         System.out.println("Hello Rikita");
+        System.out.println("Prime Testing");
     }
 }
